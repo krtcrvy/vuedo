a161dd89714bb4d098b08c448ffc83e4d206dfa2
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2025-04-04 22:41:39 +0800

    feat: add Sidebar and TopNavbar components with navigation links

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Sidebar from '@/components/layout/Sidebar.vue'
+import TopNavbar from '@/components/layout/TopNavbar.vue'
+</script>
 
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <Sidebar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavbar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
