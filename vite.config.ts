2f62da9a8fdf88fe417e7b835c5f4786666be37c
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2025-04-08 23:30:27 +0800

    feat: add auto-imports support with unplugin-auto-import; update tsconfig and integrate into Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,25 @@ import { fileURLToPath, URL } from 'node:url'
 
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter({}), vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    VueRouter({}),
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    AutoImport({
+      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.vue\.[tj]sx?\?vue/, /\.md$/],
+      imports: ['vue', 'vue-router'],
+      dts: true,
+      viteOptimizeDeps: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
